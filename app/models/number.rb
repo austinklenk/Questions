51bb87453b7e79880a7c5a9e82bb59f84680139b
Austin Klenk <austin@mastertechrv.com>
2018-01-19 20:22:39 -0500

    Added upcase validation

--- app/models/number.rb
+++ app/models/number.rb
@@ -1,3 +1,13 @@
 class Number < ApplicationRecord
 	validates :name, presence: :true
+	before_validation :uppercase_name, :uppercase_description
+
+
+	def uppercase_name
+		self.name.upcase!
+	end
+
+	def uppercase_description
+		self.description.upcase!
+	end
 end
